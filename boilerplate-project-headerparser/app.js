4d8875a020562002ea4622fe4d2e0a1e9a8736ca
vad <vadprimary@gmail.com>
2021-12-22 16:24:48 +0700

    finish 2nd test

--- boilerplate-project-headerparser/app.js
+++ boilerplate-project-headerparser/app.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+const bent = require('bent')
+const getJSON = bent('json')
+
+
+const softwareName = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+const language = "en-us";
+let ip = null;
 
 AllowCORSfromFreeCodeCamp();
 
@@ -11,8 +18,19 @@ function AllowCORSfromFreeCodeCamp() {
     });
 }
 
+app.get("/api/whoami/", async (req, res) => {
+    let ip = await GetCurrentOrCacheIP();
+    
+    res.send({ipaddress:ip, language:language,software: softwareName});
+});
 
 
 
 module.exports = app;
+
+async function GetCurrentOrCacheIP() {
+    if(ip == null)
+        ip = await getJSON('https://api.ipify.org?format=json').ip;
+    return ip;
+}
 
